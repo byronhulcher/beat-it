2f3f13dbf4cc0479155b735fada8e6ebea6243be
byronhulcher <byronhulcher@gmail.com>
2016-08-04 09:14:42 -0400

    Single quote support and fixed bookmarkle smart quotes

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,7 @@ $('blockquote p:first-of-type').unwrap()
 $('p').each(function(){$(this).text($(this).text().toLowerCase())})
 $('p').each(function(){$(this).html($(this).html().replace(/\.(?=\s[a-zA-Z]{3})/g,"<p>"))})
 $('p').each(function(){$(this).html($(this).html().replace(/[,-;:]/g,""))})
-$('p').each(function(){$(this).html($(this).html().replace(/['"“”]+/g, ''))})
+$('p').each(function(){$(this).html($(this).html().replace(/['"“”‘’]+/g, ''))}) // when converting to bookmarklet the smart quotes become %E2%80%9C%E2%80%9D%E2%80%98%E2%80%99
 $('p').each(function(){$(this).html($(this).html().replace(/[—-]+/g, ''))})
 
 $('.articleBody>p').css({'margin-left':'2em','margin-bottom':'0','text-indent':'-2em'})
