4c400a1fd69eea5313978db3a9b5b8d1498b583c
byronhulcher <byronhulcher@gmail.com>
2016-08-04 09:08:55 -0400

    Revert "Include smart single quotes"
    This reverts commit 754f9a993b19f61ddc10a1c090cad25f48b75773.

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,7 @@ $('blockquote p:first-of-type').unwrap()
 $('p').each(function(){$(this).text($(this).text().toLowerCase())})
 $('p').each(function(){$(this).html($(this).html().replace(/\.(?=\s[a-zA-Z]{3})/g,"<p>"))})
 $('p').each(function(){$(this).html($(this).html().replace(/[,-;:]/g,""))})
-$('p').each(function(){$(this).html($(this).html().replace(/['"“”‘’]+/g, ''))})
+$('p').each(function(){$(this).html($(this).html().replace(/['"“”]+/g, ''))})
 $('p').each(function(){$(this).html($(this).html().replace(/[—-]+/g, ''))})
 
 $('.articleBody>p').css({'margin-left':'2em','margin-bottom':'0','text-indent':'-2em'})
